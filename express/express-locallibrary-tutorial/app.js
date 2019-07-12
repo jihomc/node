85ac1001433fdcac578b2aa8d5bb42c81a7163d6
Michael Clegg <michaelclegg8@gmail.com>
2019-07-12 15:33:03 -0700

    Added files and changes required for deployment

--- express/express-locallibrary-tutorial/app.js
+++ express/express-locallibrary-tutorial/app.js
@@ -13,7 +13,8 @@ var app = express();
 
 // set up mongoose connection
 var mongoose = require('mongoose');
-var mongoDB = 'mongodb+srv://mongo:db@cluster0-hmcrz.mongodb.net/local_library?retryWrites=true&w=majority'
+var dev_db_url = 'mongodb+srv://mongo:db@cluster0-hmcrz.mongodb.net/local_library?retryWrites=true&w=majority';
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true });
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
